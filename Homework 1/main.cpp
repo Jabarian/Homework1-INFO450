e0f3ea63156e77ad1ab3cf8dcc5faadd414c5702
MJ <jabarianm2@vcu.edu>
2019-09-12 18:09:25 -0400

    Initial Commit

---
+++ Homework 1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Homework 1
+//
+//  Created by MJ on 9/12/19.
+//  Copyright © 2019 MJ Jabarian. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
